15ebd766329dcb98da71b89c542cee15b0a28d48
SkyVence <68406889+SkyVence@users.noreply.github.com>
2022-06-16 14:47:18 +0200

    Config.php changes

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', '!2iyFr?Ap^p9M&86');
-define('DB_NAME', 'id19103945_account');
+define('DB_USERNAME', 'id19103945_skyvence');
+define('DB_PASSWORD', 'Vnn-tEVu_tt5B~@8');
+define('DB_NAME', 'id19103945_compteweb');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
